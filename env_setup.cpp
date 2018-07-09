516b4edd32fff5cf6b6e7e3c61cd79405ebff27f
Amitava123 <mitraamitava7@gmail.com>
2018-07-09 04:59:52 +0000

    Adding automatatic updation to environment variable. Signed off by - Amitava Mitra <mitraamitava7@gmail.com>

---
+++ env_setup.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+
+void env_setup()
+{
+	int flag=0;
+	ofstream fout;
+	ifstream fin;
+	fout.open("~/.bashrc",ios::app);
+	if(fin.is_open()){
+		fout<<"\n\n# set PATH so it includes user's private bin if it exists\nif [ -d \"$HOME/bin\" ] ; then\n\tPATH=\"$HOME/bin:$PATH\"\nfi\n";
+		fout<<"\n# add Android SDK platform tools to path\nif [ -d \"$HOME/platform-tools\" ] ; then\n\tPATH=\"$HOME/platform-tools:$PATH\"\nfi";
+		flag=1;
+	}
+	fin.close();
+	fout.close();
+
+	if(flag){
+		cout<< "\nsource ~/.bashrc";
+		cin.ignore();
+		getchar();
+		system("source ~/.bashrc");
+		cout<<endl;
+	}
+}
+
+int main()
+{
+	env_setup();
+	cout<<endl;
+	return 0;
+}
