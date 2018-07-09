a095e0c1695c97454bb26795d88be0e33dd13173
Amitava123 <mitraamitava7@gmail.com>
2018-07-09 05:20:58 +0000

    again and again :p

--- env_setup.cpp
+++ env_setup.cpp
@@ -7,14 +7,12 @@ void env_setup()
 {
 	int flag=0;
 	ofstream fout;
-	ifstream fin;
 	fout.open("~/.profile",ios::app);
-	if(fin.is_open()){
+	if(fout.is_open()){
 		fout<<"\n\n# set PATH so it includes user's private bin if it exists\nif [ -d \"$HOME/bin\" ] ; then\n\tPATH=\"$HOME/bin:$PATH\"\nfi\n";
 		fout<<"\n# add Android SDK platform tools to path\nif [ -d \"$HOME/platform-tools\" ] ; then\n\tPATH=\"$HOME/platform-tools:$PATH\"\nfi";
 		flag=1;
 	}
-	fin.close();
 	fout.close();
 
 	if(flag){
